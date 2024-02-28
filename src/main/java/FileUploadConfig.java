5e2d4427a5d9a1a4e069355307b2861cef5040ed
yso <ydeveloperteam@gmail.com>
2024-02-28 18:21:26 +0100

    NICE INCHALLAH

---
+++ src/main/java/FileUploadConfig.java
@@ -0,0 +1,19 @@
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.annotation.MultipartConfig;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+@Configuration
+public class FileUploadConfig {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // Définir la taille maximale autorisée pour les fichiers téléchargés
+        factory.setMaxFileSize(DataSize.ofMegabytes(20)); // Par exemple, 10 Mo
+        factory.setMaxRequestSize(DataSize.ofMegabytes(20));
+        return factory.createMultipartConfig();
+    }
+}
